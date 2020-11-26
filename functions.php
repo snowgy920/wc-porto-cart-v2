1bcea1ca4d3563946ccef0800b44c9be780eea9f
Jay Liu <snowgy920@hotmail.com>
2020-11-26 22:26:04 +0800

    first commit

---
+++ functions.php
@@ -0,0 +1,26 @@
+<?php
+
+add_action('wp_enqueue_scripts', 'porto_child_css', 1001);
+
+// Load CSS
+function porto_child_css()
+{
+	// porto child theme styles
+	wp_deregister_style('styles-child');
+	wp_register_style('styles-child', esc_url(get_stylesheet_directory_uri()) . '/style.css');
+	wp_enqueue_style('styles-child');
+
+	if (is_rtl()) {
+		wp_deregister_style('styles-child-rtl');
+		wp_register_style('styles-child-rtl', esc_url(get_stylesheet_directory_uri()) . '/style_rtl.css');
+		wp_enqueue_style('styles-child-rtl');
+	}
+}
+
+add_action( 'wp_enqueue_scripts', 'porto_child_js', 1002 );
+function porto_child_js() {
+	wp_register_script( 'porto-child-js', esc_url( get_stylesheet_directory_uri() ) . '/custom.js', array('jquery'), '', true);
+	wp_enqueue_script( 'porto-child-js' );
+}
+
+remove_action( 'woocommerce_proceed_to_checkout', 'woocommerce_button_proceed_to_checkout', 20);
